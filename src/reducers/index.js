6bebe837f1638add9c0f7b070d7591d2329eb787
Guest User <Guest@epicodus-79.local>
2019-04-30 14:39:18 -0700

    it works

---
+++ src/reducers/index.js
@@ -0,0 +1,18 @@
+let defaultState={
+  color:'white'
+}
+
+const mainReducer = (state = defaultState, action) => {
+  if(action.type === 'CHANGE_COLOR') {
+    return {
+      ...state,
+      color: action.color
+    }
+  } else {
+    return{
+      ...state
+    }
+  }
+}
+
+export default mainReducer;
